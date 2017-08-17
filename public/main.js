822cc192558f3b8cc16046c96990d07499b1a918
laurenwelborn <lulu.welborn@gmail.com>
2017-08-16 19:00:28 -0600

    jokes on page

--- public/main.js
+++ public/main.js
@@ -1,16 +1,40 @@
-// ------ Front end js ------
+// ------ FRONT END JS ------
+
 $(document).ready(function() {
     // console.log( "ready!" );
     // ------ get data types on page from server ------ //
-    $.ajax({
-        method: 'GET',
-        url: '/jokes',
-        success: function(result) {
-            console.log("get jokes function running");
-            //get the result
-            //append somehow onto the dom? $('#moreJokes').append(jokesHtml);
-
-        }
-    })
+    function successNewJoke() {
+    	console.log("is this running?");
+	    $.ajax({
+	        method: 'GET',
+	        url: '/jokes',
+	        success: function(jokes) {
+	        	console.log(jokes);
+	        	displayJokes(jokes);
+	        },
+	        error: function(error) {
+	        	console.log(error);
+	        }
+	    })
+	}
+	successNewJoke();
 });
+
+// display one random joke - 
+// look into js random # random # between 0 and last item
+// put random number into[]
+
+// display jokes on page
+function displayJokes(jokesPassedIn) {
+	var sentenceHTML = "";
+
+	jokesPassedIn.forEach(function(joke) {
+		// take array and return string
+		sentenceHTML = sentenceHTML + '<p>' + joke.sentence + '</p>';
+	});
+
+	$('#sentence').append(sentenceHTML);
+	//append somehow onto the dom
+}
+
 
